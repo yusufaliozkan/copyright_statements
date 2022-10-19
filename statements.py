2fb0a847eb9dee6d986d09b699efa448936e4f69
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-10-19 08:05:05 +0100

    Update statements.py

--- statements.py
+++ statements.py
@@ -27,6 +27,12 @@ publisher = st.selectbox("Select a publisher:",clist)
 df_statement = df.loc[df_new['publisher']==publisher, 'statement'].values[0]
 df_statement
 
+def button():
+    pyperclip.copy(df_statement)
+
+if st.button('Copy to clipboard'):
+    button()
+
 text_to_be_copied = df_statement
 copy_dict = {"content": text_to_be_copied}
 
