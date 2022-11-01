970aecfc89be04a58aaa77766d6b7deae1b0032b
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-11-01 16:46:12 +0000

    commit

--- statements.py
+++ statements.py
@@ -17,8 +17,9 @@ path='https://upload.wikimedia.org/wikipedia/en/thumb/3/32/Logo_for_Imperial_Col
 path2 = 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b0/Copyright.svg/220px-Copyright.svg.png'
 st.image(path2, width=75)    
 st.markdown("# CAPS (Copyright and Publisher Statements) tool")
-st.markdown('### About:')
-st.write('This tool lists different copyright and publisher set statements and allows users to copy the statements to their clipboard. You can select the publisher statements from the dropdown menu or find the Frequently used publisher statements. You can also find an example of rights retention statement if you wish to add it to your submitted manuscript. The tool also has a quick citation generator for ‘grey literature’ items.')
+st.markdown('#### About:')
+st.write('This tool lists different copyright and publisher set statements and allows users to copy the statements to their clipboard. You can select the publisher statements from the dropdown menu or find the [Frequently used publisher statements](https://copyright-statements.streamlitapp.com/#frequently-used-publisher-statements). You can also find an example of rights retention statement if you wish to add it to your submitted manuscript. The tool also has a quick citation generator for ‘grey literature’ items.')
+st.write('You can find the contact information, source code of this tool, and disclaimer at the bottom of this page.')
 
 st.sidebar.image(path2, width=150)
 st.sidebar.markdown("# CAPS tool")
