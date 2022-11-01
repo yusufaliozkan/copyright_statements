e458c46bf58a40cabd4683c377cb0e4ca5d2f5f1
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-11-01 20:25:47 +0000

    commit

--- statements.py
+++ statements.py
@@ -25,17 +25,6 @@ with st.sidebar:
         st.write('This tool lists different copyright and publisher set statements and allows users to copy the statements to their clipboard. You can select the publisher statements from the dropdown menu or find the Frequently used publisher statements. You can also find an example of rights retention statement if you wish to add it to your submitted manuscript. The tool also has a quick citation generator for ‘grey literature’ items.')
         st.write('To see the contact information, source code of this tool, and disclaimer, click **More** at the bottom of this page.')
 
-
-with st.expander('About'):
-    tab1, tab2 = st.tabs(['tab1', 'tab2'])
-    with tab1:
-        st.write('This tool lists different copyright and publisher set statements and allows users to copy the statements to their clipboard. You can select the publisher statements from the dropdown menu or find the Frequently used publisher statements. You can also find an example of rights retention statement if you wish to add it to your submitted manuscript. The tool also has a quick citation generator for ‘grey literature’ items.')
-    with tab2:
-        st.write('To see the contact information, source code of this tool, and disclaimer, click **More** at the bottom of this page.')
-
-st.sidebar.image(path2, width=150)
-st.sidebar.markdown("# CAPS tool")
-
 tab1, tab2, tab3 = st.tabs(['Publisher and copyright statements', 'Rights retention statement', 'Grey literature citation generator'])
 
 with tab1:
