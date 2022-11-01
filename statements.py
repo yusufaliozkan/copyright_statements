accc1a26bfc9761af34d671b7bfe4aef90d58294
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-11-01 20:24:09 +0000

    commit

--- statements.py
+++ statements.py
@@ -19,6 +19,8 @@ st.image(path2, width=75)
 st.markdown("# CAPS (Copyright and Publisher Statements) tool")
 
 with st.sidebar:
+    st.image(path2, width=150)
+    st.markdown("# CAPS tool")    
     st.write('This tool lists different copyright and publisher set statements and allows users to copy the statements to their clipboard. You can select the publisher statements from the dropdown menu or find the Frequently used publisher statements. You can also find an example of rights retention statement if you wish to add it to your submitted manuscript. The tool also has a quick citation generator for ‘grey literature’ items.')
     st.write('To see the contact information, source code of this tool, and disclaimer, click **More** at the bottom of this page.')
 
