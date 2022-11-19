252eeb3b0729c6ce7dd186e105c933040e85b007
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-11-19 18:45:39 +0000

    commit

--- statements.py
+++ statements.py
@@ -389,7 +389,7 @@ with tab2:
     st.write("[Rights retention statement](https://www.coalition-s.org/resources/rights-retention-strategy/) allows authors to exercise the rights of their accepted manuscripts. Copy the statement below and paste into your submitted version.")
     st.info('**'+df.loc[df_rrs['publisher']=='Rights retention statement', 'statement'].values[0]+'**')
     df_rrs
-    text_to_be_copied = df_rrs.loc[df_new['publisher']=='Rights retention statement', 'statement'].values[0]
+    text_to_be_copied = df_rrs.loc[df_rrs['publisher']=='Rights retention statement', 'statement'].values[0]
     copy_dict = {"content": text_to_be_copied}
 
     copy_button = Button(label="Copy to clipboard")
