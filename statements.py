969c14ef802f972310440c18f0d0ea71389e5eea
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2022-11-23 20:58:06 +0000

    commit

--- statements.py
+++ statements.py
@@ -11,7 +11,8 @@ import streamlit.components.v1 as components
 df = pd.read_csv(r'statements.csv')
 df['publisher'] = df['publisher'].astype(str)
 df['link'] = df['link'].astype(str)
-df['link'] = df['link'].replace(' ', 'None',inplace=True)
+df['link'] = df['link'].replace(' ', 'None')
+# df.link = df.link.fillna('none')
 
 df_rrs = df.copy()
 df['statement'] = 'Copyright ' + df['statement'].astype(str)
