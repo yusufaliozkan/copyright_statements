64b585cdd4a89600816937e953d0190cfdc3ab5f
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-03-30 17:10:40 +0100

    sd

--- statements.py
+++ statements.py
@@ -229,6 +229,7 @@ with tab3:
 with tab4:
     st.subheader('Title organiser')
     title = st.text_input('Write the title here:')
+    st.write(title)
 
 with st.expander("Sherpa Romeo"):
     components.iframe("https://v2.sherpa.ac.uk/romeo/", height=1500)
