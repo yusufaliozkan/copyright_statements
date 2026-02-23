91276058326665eb75df524746ef51c0a6a33df2
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2026-02-23 15:22:05 +0000

    sd

--- statements.py
+++ statements.py
@@ -157,22 +157,7 @@ with tab3:
     if st.button('Show citation'):
         st.write('**Citation** : ')
         st.info(citation)
-
-        text_to_be_copied = citation
-        copy_dict = {"content": text_to_be_copied}
-
-        copy_button = Button(label="Copy to clipboard")
-        copy_button.js_on_event("button_click", CustomJS(args=copy_dict, code="""
-        navigator.clipboard.writeText(content);
-        """))
-
-        no_event = streamlit_bokeh_events(
-        copy_button,
-        events="GET_TEXTcite",
-        key="get_textcite",
-        refresh_on_update=True,
-        override_height=75,
-        debounce_time=0)   
+        st_copy_to_clipboard(citation, key="copy_citation")  
 
 with tab4:
     st.subheader('Title organiser')    
@@ -228,20 +213,7 @@ with tab4:
         if title:
             decapitalized_title = decapitalize_titles(title)
             st.write(f"**Decapitalized Title:** {decapitalized_title}")
-
-            copy_dict = {"content": decapitalized_title}
-            copy_button = Button(label="Copy to clipboard")
-            copy_button.js_on_event("button_click", CustomJS(args=copy_dict, code="""
-                navigator.clipboard.writeText(content);
-                """))
-
-            no_event = streamlit_bokeh_events(
-                copy_button,
-                events="GET_title",
-                key="get_title",
-                refresh_on_update=True,
-                override_height=75,
-                debounce_time=0)
+            st_copy_to_clipboard(decapitalized_title, key="copy_title")
 
 
 
